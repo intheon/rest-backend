b85880355e507a43cc8d065b413d196f9ae782b8
intheon <allobon@gmail.com>
2015-11-25 20:14:59 +0000

    added login!

--- api/core.php
+++ api/core.php
@@ -1,120 +1,172 @@
 <?php
 
-require "/vendor/autoload.php";										// loads our frameworks
-require "/database.php";											// loads our database handling code
+require "/vendor/autoload.php";															// loads our frameworks
+require "/database.php";																// loads our database handling code
 
 class apiHandler
 {
 	public function __construct()
 	{
-		$app = new \Slim\Slim();									// slim framework
+		$app = new \Slim\Slim();														// slim framework
 
 		/*
 		Defining all the routes below.
 		See https://github.com/intheon/rest-backend#allowed-methods for a full list.
 		*/
 
-		$app->get("/", array($this, "readHomeRoute"));				// for the root
-		$app->get("/user", array($this, "readAllUsers"));			// get all users
-		$app->get("/widget", array($this, "readAllWidgets")); 		// get all widgets
-		$app->get("/user/:id", array($this, "readOneUser"));		// get specific user
-		$app->get("/widget/:id", array($this, "readOneWidget"));	// get specific widget
-		$app->get("/state/:id", array($this, "readOneState"));		// get specific state
-		$app->post("/user", array($this, "createUser"));			// create new user
-		$app->post("/widget", array($this, "createWidget"));		// create new widget
-		$app->post("/state", array($this, "createState"));			// create new state
-		$app->put("/user/:id", array($this, "updateUser"));			// update user
-		$app->put("/widget/:id", array($this, "updateWidget"));		// update widget
-		$app->put("/state/:id", array($this, "updateState"));		// update state
-		$app->delete("/user/:id", array($this, "deleteUser"));		// delete user
-		$app->delete("/widget/:id", array($this, "deleteWidget"));	// delete widget
-		$app->delete("/state/:id", array($this, "deleteState"));	// delete state
+		$app->get("/", array($this, "readHomeRoute"));									// for the root
+		$app->get("/user", array($this, "readAllUsers"));								// get all users
+		$app->get("/widget", array($this, "readAllWidgets")); 							// get all widgets
+		$app->get("/user/:id", array($this, "readOneUser"));							// get specific user
+		$app->get("/widget/:id", array($this, "readOneWidget"));						// get specific widget
+		$app->get("/state/:id", array($this, "readOneState"));							// get specific state
+		$app->post("/login/:username/:password", array($this, "loginUser"));			// create new user
+		$app->post("/user", array($this, "createUser"));								// create new user
+		$app->post("/widget", array($this, "createWidget"));							// create new widget
+		$app->post("/state", array($this, "createState"));								// create new state
+		$app->put("/user/:id", array($this, "updateUser"));								// update user
+		$app->put("/widget/:id", array($this, "updateWidget"));							// update widget
+		$app->put("/state/:id", array($this, "updateState"));							// update state
+		$app->delete("/user/:id", array($this, "deleteUser"));							// delete user
+		$app->delete("/widget/:id", array($this, "deleteWidget"));						// delete widget
+		$app->delete("/state/:id", array($this, "deleteState"));						// delete state
 
-		$app->run();												// start this mofo
+		$app->run();																	// start this mofo
 	}
 
-	public function readHomeRoute()									// messages for the home screen
+	public function readHomeRoute()														// messages for the home screen
 	{
 		echo "root mofo";
 	}
 
-	public function readAllUsers()									// block of users
+	public function readAllUsers()														// block of users
 	{
 		$db = new database();
 		$data = $db->connectToDB()->select("user","*");
 		echo json_encode($data);
-		return false;
 	}
 
-	public function readAllWidgets()								// block of widgets
+	public function readAllWidgets()													// get block of widgets
 	{
-
 		echo "all widgets...";
 	}
 
-	public function readOneUser($id)								// users profile
+	public function readOneUser($id)													// get users profile
 	{
-		$db = new database();
-
-
+		//$db = new database();
 		echo "a specific user... " . $id;
-
 	}
 
-	public function readOneWidget($id)								// widget info
+	public function readOneWidget($id)													// get widget info
 	{
-
 		echo "a specific widget... " . $id;
 	}
 
-	public function readOneState($id)								// specific state details
+	public function readOneState($id)													// get specific state details
 	{
 		echo "a specific state... " . $id;
 	}
 
-	public function createUser()									// create a new user
+	public function createUser()														// create a new user
 	{
 		echo "user created";
 	}
 
-	public function createWidget()									// create new widget
+	public function createWidget()														// create new widget
 	{
 		echo "widget created";
 	}
 
-	public function createState()									// create new state
+	public function createState()														// create new state
 	{
 		echo "state created";
 	}
 
-	public function updateUser()									// update existing user
+	public function loginUser($username, $password)										// log user in
+	{
+		$doesUserExist = $this->checkUsernameExists($username);
+
+		if ($doesUserExist)
+		{
+			$isPasswordCorrect = $this->checkPassword($username, $password);
+			if ($isPasswordCorrect) $this->generateAuthToken($username);
+			else if ($isPasswordCorrect == false) echo htmlspecialchars("incorrectpw");
+		}
+		else echo htmlspecialchars("nonexistent");
+	}
+
+	public function updateUser()														// update existing user
 	{
 		echo "user updated";
 	}
 
-	public function updateWidget()									// update existing widget
+	public function updateWidget()														// update existing widget
 	{
 		echo "widget updated";
 	}
 
-	public function updateState()									// update existing state
+	public function updateState()														// update existing state
 	{
 		echo "state updated";
 	}
 
-	public function deleteUser()									// delete existing user
+	public function deleteUser()														// delete existing user
 	{
 		echo "user deleted";
 	}
 
-	public function deleteWidget()									// delete existing widget
+	public function deleteWidget()														// delete existing widget
 	{
 		echo "widget deleted";
 	}
 
-	public function deleteState()									// delete existing state
+	public function deleteState()														// delete existing state
 	{
 		echo "state deleted";
+	}
+
+	private function hashPassword($plaintext_password)									// helper functions for log in and auth stuff
+	{
+		return password_hash($plaintext_password,PASSWORD_DEFAULT);
+	}
+
+	private function checkUsernameExists($username)
+	{
+		$db = new database();
+		$data = $db->connectToDB()->select("auth", "username", ["username" => $username]);
+
+		if (count($data) === 0) return false;
+		else if (count($data) === 1) return true;
+	}
+
+	private function checkPassword($username, $password)
+	{
+		$db = new database();
+		$data = $db->connectToDB()->select("auth", "password", ["username" => $username]);
+
+		foreach ($data as $row)
+			if (!password_verify($password,$row)) return false;
+			else if (password_verify($password,$row)) return true;
+	}
+
+	private function generateAuthToken($username)
+	{
+		/*
+		see https://www.webstreaming.com.ar/articles/php-slim-token-authentication/
+		*/
+		$credentials["username"] = $username;
+		$credentials["token"] = bin2hex(openssl_random_pseudo_bytes(16));
+		$credentials["tokenExpiration"] = date('Y-m-d H:i:s', strtotime('+1 hour'));
+
+		$db = new database();
+		$data = $db->connectToDB()->update("auth", [
+				"token" => $credentials["token"],
+				"token_expiry" => $credentials["tokenExpiration"]
+			],[
+				"username" => $username
+			]);
+
+		echo json_encode($credentials);
 	}
 }
 
